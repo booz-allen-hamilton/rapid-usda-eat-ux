91e9dfc4f9e17607d7cc9e07a06cd251fdd717a9
Daniel Rich <danielrich@Daniels-MBP.fios-router.home>
2016-02-17 12:54:37 -0500

    added styling to contact info page

--- application/views/pages/application-form/contactInformation.php
+++ application/views/pages/application-form/contactInformation.php
@@ -1,47 +1,67 @@
 <div class="row text-center">
-	<div class="col-sm-12">
+	<div class="col-sm-12 col-md-offset-1 col-md-10">
 		<h2 class="mb25">Contact Information</h2>
-		<p>
+		<h4>
 			Enter your contact information and how you would like us to communicate with you.
 			<a href="#" data-toggle="modal" data-target="#childrenClarification">
 				<span class="glyphicon glyphicon-info-sign"></span>
 			</a>
-		</p>
+		</h4>
 	</div>
 </div>
-<div class="row">
-	<div class="col-sm-8">
+<div class="row" style="margin-top: 50px;">
+	<div class="col-sm-8 col-md-offset-1 col-md-7">
 		<label>Street Address</label>
 		<input type="text" class="form-control input-lg" />
 	</div>
-	<div class="col-sm-4">
+	<div class="col-sm-4 col-md-3">
 		<label>Apt #</label>
 		<input type="text" class="form-control input-lg" />
 	</div>
 </div>
 
-<div class="row">
-	<div class="col-sm-4">
-		<label>City</label>
+<div class="row" style="margin-top: 20px;">
+	<div class="col-sm-4 col-md-offset-1 col-md-4">
+		<label>*City</label>
 		<input type="text" class="form-control input-lg" />
 	</div>
-	<div class="col-sm-4">
-		<label>State</label>
+	<div class="col-sm-4 col-md-4">
+		<label>*State</label>
 		<input type="text" class="form-control input-lg" />
 	</div>
-	<div class="col-sm-4">
-		<label>Zip</label>
+	<div class="col-sm-4 col-md-2">
+		<label>*Zip</label>
 		<input type="text" class="form-control input-lg" />
 	</div>
 </div>
 
-<div class="row">
-	<div class="col-sm-3">
+<div class="row" style="margin-top: 20px;">
+	<div class="col-sm-3 col-md-offset-1 col-md-3">
 		<label>Phone</label>
 		<input type="text" class="form-control input-lg" />
 	</div>
-	<div class="col-sm-7">
+	<div class="col-sm-7 col-md-7">
 		<label>Email</label>
 		<input type="text" class="form-control input-lg" />
+	</div>
+</div>
+
+<div class="row" style="margin-top: 20px;">
+	<div class="col-sm-3 col-md-offset-1 col-md-10">
+		<label>Send status updates through:</label>
+	</div>
+</div>
+<div class="row">
+	<div class="col-sm-3 col-md-offset-1 col-md-2">
+		<label class="checkbox-inline">
+			<input type="checkbox" name="status_text">
+			<span class="checkbox-label">Text message</span>
+		</label>
+	</div>
+	<div class="col-sm-7 col-md-3">
+		<label class="checkbox-inline">
+			<input type="checkbox" name="status_email">
+			<span class="checkbox-label">Email</span>
+		</label>
 	</div>
 </div>
