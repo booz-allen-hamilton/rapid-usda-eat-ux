99eb5c8e3fc35d2f4aa66d864c25c940e1372298
Daniel Rich <danielrich@Daniels-MacBook-Pro.local>
2016-02-23 13:28:25 -0500

    corrected form validation for electronicSignature

--- application/views/pages/application-form/electronicSignature.php
+++ application/views/pages/application-form/electronicSignature.php
@@ -15,39 +15,43 @@
 			<?=help_icon($this->lang->line('electronic_sig_subtitle_help'))?>
 		</p>
 		<div class="row">
-			<div class="col-sm-12 col-md-1">
-				<label class="checkbox-inline">
-					<input type="checkbox" name="electronicSignature" data-error="<?=$this->lang->line('error_electronic_sig_agree')?>" required>
-				</label>
-			</div>
-			<div class="col-sm-12 col-md-11 text-left sig-statement">
-				<p><?=$this->lang->line('electronic_sig_statement')?></p>
-				<input type="text"
-					class="form-control input-lg"
-					name="electronic_signature"
-					id="electronic_signature"
-					data-match="#electronic_signature_match"
-					data-error="<?=$this->lang->line('error_electronic_sig_name')?>"
-					required />
-				<div class="help-block with-errors"></div>
-				<span><?=$primary_name?></span>
-				<input type="hidden" id="electronic_signature_match" value="<?=$primary_name?>" />
+			<div class="form-group">
+				<div class="col-sm-12 col-md-1">
+					<label class="checkbox-inline">
+						<input type="checkbox" name="electronicSignature" data-error="<?=$this->lang->line('error_electronic_sig_agree')?>" required>
+					</label>
+				</div>
+				<div class="col-sm-12 col-md-11 text-left sig-statement">
+					<p><?=$this->lang->line('electronic_sig_statement')?></p>
+					<input type="text"
+						class="form-control input-lg"
+						name="electronic_signature"
+						id="electronic_signature"
+						data-match="#electronic_signature_match"
+						data-error="<?=$this->lang->line('error_electronic_sig_name')?>"
+						required />
+					<div class="help-block with-errors"></div>
+					<span><?=$primary_name?></span>
+					<input type="hidden" id="electronic_signature_match" value="<?=$primary_name?>" />
+				</div>
 			</div>
 		</div>
-		<div class="row mt-20">
-			<div class="col-sm-12 col-md-offset-1 col-md-11 text-left">
-				<h4>Last 4 digits of Social Security Number <?=help_icon($this->lang->line('electronic_sig_ssn_help'))?></h4>
-			</div>
-			<div class="col-sm-12 col-md-offset-1 col-md-4 text-left">
-				<span>XXX-XX-</span>
-				<input type="text" class="form-control input-lg ssn" data-minlength="4" name="social_security_last_four" maxlength="4" required data-error="<?=$this->lang->line('error_electronic_sig_social')?>" />
-			</div>
-			<div class="col-sm-12 col-md-4 text-left">
-				<label class="checkbox-inline ssn-checkbox">
-					<input type="checkbox" name="ssn_not_available" value="1">
-					<span class="checkbox-label"><?=$this->lang->line('electronic_sig_not_available')?></span>
+		<div class="form-group">
+			<div class="row mt-20">
+				<div class="col-sm-12 col-md-offset-1 col-md-11 text-left">
+					<h4>Last 4 digits of Social Security Number <?=help_icon($this->lang->line('electronic_sig_ssn_help'))?></h4>
+				</div>
+				<div class="col-sm-12 col-md-offset-1 col-md-4 text-left">
+					<span>XXX-XX-</span>
+					<input type="text" class="form-control input-lg ssn" data-minlength="4" name="social_security_last_four" maxlength="4" required data-error="<?=$this->lang->line('error_electronic_sig_social')?>" />
 					<div class="help-block with-errors"></div>
-				</label>
+				</div>
+				<div class="col-sm-12 col-md-4 text-left">
+					<label class="checkbox-inline ssn-checkbox">
+						<input type="checkbox" name="ssn_not_available" value="1">
+						<span class="checkbox-label"><?=$this->lang->line('electronic_sig_not_available')?></span>
+					</label>
+				</div>
 			</div>
 		</div>
 	</div>
--- assets/js/form/electronicSignature.js
+++ assets/js/form/electronicSignature.js
@@ -8,11 +8,10 @@ $(function(){
 	});
 
 	$(document).on('keyup', '[name="social_security_last_four"]', function(){
-		if ($(this).val().length == 4) {
+		if ($(this).val().length === 4) {
 			$('[name="ssn_not_available"]').val(0).removeAttr('required').parents('.ssn-checkbox').addClass('hide');
 		} else {
 			$('[name="ssn_not_available"]').val(1).attr('required', 'true').parents('.ssn-checkbox').removeClass('hide');
 		}
-		$('#form-application').validator('validate');
 	});
 });
