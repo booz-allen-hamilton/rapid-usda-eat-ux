18166e65008971330d3115b8c59afc61f2e7c8c0
Tony Chen <tonychen091@gmail.com>
2016-02-29 21:38:28 -0500

    Adding Google Analytics for Tracking

--- application/views/layout/application.php
+++ application/views/layout/application.php
@@ -47,5 +47,15 @@
 		var _elev = window._elev || {};(function() {var i,e;i=document.createElement("script"),i.type='text/javascript';i.async=1,i.src="https://static.elev.io/js/v3.js",e=document.getElementsByTagName("script")[0],e.parentNode.insertBefore(i,e);})();
 		_elev.account_id = '56cf136c43a7a';
 	</script>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-63076956-3', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 </body>
 </html>
